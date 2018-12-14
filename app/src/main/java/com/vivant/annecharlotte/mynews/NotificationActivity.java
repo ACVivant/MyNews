c54489ab429706f0ec5c293fead967dca9783220
ACVivant <annecharlotte.vivant@gmail.com>
2018-12-14 12:37:21 +0100

    mise en place des differences visuelles entre les vues Search et Notifications

--- app/src/main/java/com/vivant/annecharlotte/mynews/NotificationActivity.java
+++ app/src/main/java/com/vivant/annecharlotte/mynews/NotificationActivity.java
@@ -5,11 +5,19 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 /**
  * Created by Anne-Charlotte Vivant on 14/12/2018.
  */
 public class NotificationActivity extends AppCompatActivity {
+
+    private Button searchButton;
+
+    private LinearLayout beginDate,
+                         endDate;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,5 +40,13 @@ public class NotificationActivity extends AppCompatActivity {
 
     private void configureWindow() {
 
+        searchButton = (Button) findViewById(R.id.search_button);
+        searchButton.setVisibility(View.GONE);
+
+        beginDate = (LinearLayout) findViewById(R.id.begin_date);
+        beginDate.setVisibility(View.GONE);
+
+        endDate = (LinearLayout) findViewById(R.id.end_date);
+        endDate.setVisibility(View.GONE);
     }
 }
--- app/src/main/java/com/vivant/annecharlotte/mynews/SearchActivity.java
+++ app/src/main/java/com/vivant/annecharlotte/mynews/SearchActivity.java
@@ -1,15 +1,34 @@
 package com.vivant.annecharlotte.mynews;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by Anne-Charlotte Vivant on 14/12/2018.
          */
 public class SearchActivity extends AppCompatActivity {
+
+    private EditText mEditText_beginDate,
+                     mEditText_endDate;
+
+    private Calendar mCalendar;
+
+    private String mBeginDate, mEndDate;
+
+    private Switch notificationSwitch;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,6 +36,8 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.search_notif_window);
         this.configureSearchToolbar();
         this.configureWindow();
+        initializeOnClickBeginDateListener();
+        initializeOnClickEndDateListener();
     }
 
     private void configureSearchToolbar() {
@@ -31,6 +52,103 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     private void configureWindow() {
+        notificationSwitch = (Switch) findViewById(R.id.switch_notification);
+        notificationSwitch.setVisibility(View.GONE);
+    }
 
+    // -----------------------------------------------------------
+    // BEGIN DATE : Text View Listener + DatePicker + Label update
+    // -----------------------------------------------------------
+
+    // Attach listener to TextView that calls a DatePickerDialog when clicked on
+    private void initializeOnClickBeginDateListener() {
+        mEditText_beginDate = findViewById(R.id.begin_date_edittext);
+        mCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                mCalendar.set(Calendar.YEAR, year);
+                mCalendar.set(Calendar.MONTH, monthOfYear);
+                mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateBeginDateLabel();
+            }
+
+        };
+
+        mEditText_beginDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(SearchActivity.this, date, mCalendar
+                        .get(Calendar.YEAR), mCalendar.get(Calendar.MONTH),
+                        mCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+    }
+
+    // Update text field value with the chosen Date, date displayed should be: dd/MM/yyyy, for example 10/03/2018 for 10th March 2018
+    private void updateBeginDateLabel() {
+        String mFormat = "dd/MM/yyyy";
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat(mFormat, Locale.US);
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd");
+
+        mEditText_beginDate.setText(sdf.format(mCalendar.getTime()));
+        mBeginDate = sdf2.format(mCalendar.getTime());
+    }
+
+    // Sets date value to null
+    private void nullifyBeginDate() {
+        mBeginDate = null;
+    }
+
+    // ---------------------------------------------------------
+    // END DATE : TextView Listener + DatePicker + Label update
+    // ---------------------------------------------------------
+
+    // Attach listener to TextView that calls a DatePickerDialog when clicked on
+    private void initializeOnClickEndDateListener() {
+
+        mEditText_endDate = findViewById(R.id.end_date_edittext);
+        mCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                mCalendar.set(Calendar.YEAR, year);
+                mCalendar.set(Calendar.MONTH, monthOfYear);
+                mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateEndDateLabel();
+            }
+        };
+
+        mEditText_endDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(SearchActivity.this, date, mCalendar
+                        .get(Calendar.YEAR), mCalendar.get(Calendar.MONTH),
+                        mCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+    }
+
+    // Update text field value with the chosen Date, date displayed should be: dd/MM/yyyy, for example 10/03/2018 for 10th March 2018
+    private void updateEndDateLabel() {
+        String dateFormat = "MM/dd/yyyy";
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.US);
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd");
+
+        mEditText_endDate.setText(sdf.format(mCalendar.getTime()));
+        mEndDate = sdf2.format(mCalendar.getTime());
+
+    }
+
+    // Sets date value to null
+    private void nullifyEndDate() {
+        mEndDate = null;
     }
 }
