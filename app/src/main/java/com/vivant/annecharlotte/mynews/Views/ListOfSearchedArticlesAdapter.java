52256aad90308447a2c528cb7d5ef2ad84f8307c
ACVivant <annecharlotte.vivant@gmail.com>
2018-12-20 14:10:33 +0100

    mise en route de MostPopular et Business

---
+++ app/src/main/java/com/vivant/annecharlotte/mynews/Views/ListOfSearchedArticlesAdapter.java
@@ -0,0 +1,64 @@
+package com.vivant.annecharlotte.mynews.Views;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.RequestManager;
+import com.vivant.annecharlotte.mynews.Models.NYTTopStoriesArticles;
+import com.vivant.annecharlotte.mynews.Models.ResultArticles;
+import com.vivant.annecharlotte.mynews.Models.ResultTopStories;
+import com.vivant.annecharlotte.mynews.R;
+import com.vivant.annecharlotte.mynews.TopStoriesPageFragment;
+
+import java.util.List;
+
+/**
+ * Created by Anne-Charlotte Vivant on 18/12/2018.
+ */
+public class ListOfArticlesAdapter extends RecyclerView.Adapter<ListOfArticlesViewHolder> {
+
+    private List<ResultArticles> listOfArticles;
+    private RequestManager glide;
+    private OnItemClickedListener mListener;
+    private String apiTag;
+
+    public interface OnItemClickedListener{
+        void OnItemClicked(int position);
+    }
+
+    public void setOnItemClickedListener(OnItemClickedListener listener) {
+        mListener = listener;
+    }
+
+    // CONSTRUCTOR
+        public ListOfArticlesAdapter(List<ResultArticles> listOfArticles, RequestManager glide, String apiTag) {
+        this.listOfArticles = listOfArticles;
+        this.glide = glide;
+        this.apiTag =  apiTag;
+    }
+
+    @Override
+    public ListOfArticlesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // CREATE VIEW HOLDER AND INFLATING ITS XML LAYOUT
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.fragment_item, parent, false);
+        return new ListOfArticlesViewHolder(view, mListener, apiTag);
+    }
+
+    // UPDATE VIEW HOLDER
+    @Override
+    public void onBindViewHolder(ListOfArticlesViewHolder viewHolder, int position) {
+        viewHolder.updateWithNYTArticles(this.listOfArticles.get(position), this.glide);
+        //viewHolder.updateWithNYTArticles(this.listOfArticles.get(position));
+    }
+
+    // RETURN THE TOTAL COUNT OF ITEMS IN THE LIST
+    @Override
+    public int getItemCount() {
+        return this.listOfArticles.size();
+    }
+}
---
+++ app/src/main/java/com/vivant/annecharlotte/mynews/Views/ListOfSearchedArticlesViewHolder.java
@@ -0,0 +1,78 @@
+package com.vivant.annecharlotte.mynews.Views;
+
+import android.graphics.drawable.Icon;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.vivant.annecharlotte.mynews.Models.NYTTopStoriesArticles;
+import com.vivant.annecharlotte.mynews.Models.ResultArticles;
+import com.vivant.annecharlotte.mynews.Models.ResultTopStories;
+import com.vivant.annecharlotte.mynews.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Anne-Charlotte Vivant on 18/12/2018.
+ */
+public class ListOfArticlesViewHolder extends RecyclerView.ViewHolder{
+
+    @BindView(R.id.fragment_item_title) TextView titleTextView;
+    @BindView(R.id.fragment_item_date) TextView dateTextView;
+    @BindView(R.id.fragment_item_section) TextView sectionTextView;
+    @BindView(R.id.fragment_item_image) ImageView imageView;
+
+    String apiTag;
+
+    public ListOfArticlesViewHolder(View itemView, final ListOfArticlesAdapter.OnItemClickedListener listener, String apiTag) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(listener!=null) {
+                    int position = getAdapterPosition();
+                    if (position!= RecyclerView.NO_POSITION) {
+                        listener.OnItemClicked(position);
+                    }
+                }
+            }
+        });
+        this.apiTag = apiTag;
+    }
+
+
+       public void updateWithNYTArticles(ResultArticles NYTArticle, RequestManager glide){
+        this.titleTextView.setText(NYTArticle.getTitle());
+
+        String date = NYTArticle.getPublishedDate().substring(0,9);
+        this.dateTextView.setText(date);
+
+        String section = NYTArticle.getSection() ;
+
+        if (apiTag=="TOPSTORIES") {  // ici on gère le fait qu'il n'y a pas de subsection dans MostPopular
+        if(NYTArticle.getSubsection().length()> 0){
+                section += ">" +NYTArticle.getSubsection();
+        }
+        }
+        this.sectionTextView.setText(section);
+
+           if (apiTag=="TOPSTORIES") {
+        if (NYTArticle.getMultimedia().size()>0){  // ici il faut gérer les cas où Multimedia est un tableau vide
+        glide.load(NYTArticle.getMultimedia().get(0).getUrl()).into(imageView);}
+        else
+               this.imageView.setImageResource(R.drawable.ic_menu_camera);
+           }
+
+           if (apiTag=="MOSTPOPULAR") {
+               if (NYTArticle.getMedia().size()>0){  // ici il faut gérer les cas où Media est un tableau vide
+                   glide.load(NYTArticle.getMedia().get(0).getMediaMetadata().get(0).getUrl()).into(imageView);}
+               else
+                   this.imageView.setImageResource(R.drawable.ic_menu_camera);
+           }
+    }
+}
